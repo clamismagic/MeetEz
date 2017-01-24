7ac28b5d09c2e7d8f64ecd917ff57d48284c9d37
chsiang469 <chanhonsiang469@gmail.com>
2017-01-24 11:52:14 +0800

    dashboard

---
+++ app/src/main/java/name/calvin/meetez/Dashboard.java
@@ -0,0 +1,17 @@
+package com.asd.asd.assignment1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Asd on 30/11/2016.
+ */
+
+public class Dashboard extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
