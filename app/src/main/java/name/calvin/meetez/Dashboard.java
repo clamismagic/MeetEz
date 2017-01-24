002ba30269f6f306ee688d1b9a5ca0c102c8ac4c
Matt1998 <tsekinping2010@gmail.com>
2017-01-24 12:13:12 +0800

    Updates

--- app/src/main/java/name/calvin/meetez/Dashboard.java
+++ app/src/main/java/name/calvin/meetez/Dashboard.java
@@ -1,13 +1,15 @@
-package com.asd.asd.assignment1;
+package name.calvin.meetez;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import name.calvin.meetez.R;
 
 /**
  * Created by Asd on 30/11/2016.
  */
 
-public class Dashboard extends AppCompatActivity{
+public class Dashboard extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
