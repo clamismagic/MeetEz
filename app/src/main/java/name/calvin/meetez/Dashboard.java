00495a693e1bb8e24562f83e8b813c9e797c2df9
Matt1998 <tsekinping2010@gmail.com>
2017-01-24 12:16:55 +0800

    UPDATED!

--- app/src/main/java/name/calvin/meetez/Dashboard.java
+++ app/src/main/java/name/calvin/meetez/Dashboard.java
@@ -3,12 +3,6 @@ package name.calvin.meetez;
 import android.app.Activity;
 import android.os.Bundle;
 
-import name.calvin.meetez.R;
-
-/**
- * Created by Asd on 30/11/2016.
- */
-
 public class Dashboard extends Activity {
 
     @Override
