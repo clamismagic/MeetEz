1ee7490efd8d68fac5b8ac05ad4cb527bf7d7857
Matt1998 <tsekinping2010@gmail.com>
2017-01-31 11:38:13 +0800

    Added string "Participants" to strings.xml Created Participants.java and its layout file participants.xml Updated AndroidManifest

---
+++ app/src/main/java/name/calvin/meetez/Participants.java
@@ -0,0 +1,13 @@
+package name.calvin.meetez;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Participants extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.participants);
+    }
+}
