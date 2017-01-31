7d8eaacb0e181a9489290631d502fe4cf6aecdde
chsiang469 <chanhonsiang469@gmail.com>
2017-01-31 11:43:45 +0800

    description

--- app/src/main/java/name/calvin/meetez/Description.java
+++ app/src/main/java/name/calvin/meetez/Description.java
@@ -2,14 +2,53 @@ package name.calvin.meetez;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class Description extends Activity {
+public class Description extends Activity implements OnClickListener{
+
+    private Button delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.);
+        setContentView(R.layout.description);
+        delete=(Button)findViewById(R.id.delete);
+        delete.setOnClickListener(this);
     }
 
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.backout:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Are you sure you want to back out of this event?");
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Do nothing
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+        }
+    }
 }
