bf3abd2ec6fd695ce4e7c86698a1b87d2b7c21e9
chsiang469 <chanhonsiang469@gmail.com>
2017-01-31 11:54:47 +0800

    description

--- app/src/main/java/name/calvin/meetez/Description.java
+++ app/src/main/java/name/calvin/meetez/Description.java
@@ -4,6 +4,7 @@ package name.calvin.meetez;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ import android.view.View.OnClickListener;
 public class Description extends Activity implements OnClickListener{
 
     private Button delete;
+    private Button editParticipants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,27 @@ public class Description extends Activity implements OnClickListener{
         setContentView(R.layout.description);
         delete=(Button)findViewById(R.id.delete);
         delete.setOnClickListener(this);
+        editParticipants=(Button)findViewById(R.id.editparticipants);
+        editParticipants.setOnClickListener(click_listener);
     }
 
+    private OnClickListener click_listener = new OnClickListener() {
 
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = null;
+            switch (view.getId()) {
+                case R.id.editparticipants:
+                    intent = new Intent(getApplicationContext(), Participants.class);
+                    if (intent != null) {
+                        startActivity(intent);
+                    }
+                    break;
+            }
+        }
+
+    };
 
     public void onClick(View v) {
         switch (v.getId()) {
