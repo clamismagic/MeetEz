e33724eb5c845ef3f403ecb3387a151fc5209156
Matt1998 <tsekinping2010@gmail.com>
2017-01-31 13:48:39 +0800

    Implemented OnClickListener() for Participants.java

--- app/src/main/java/name/calvin/meetez/Participants.java
+++ app/src/main/java/name/calvin/meetez/Participants.java
@@ -1,13 +1,52 @@
 package name.calvin.meetez;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class Participants extends Activity {
+public class Participants extends Activity implements OnClickListener{
+
+    private ImageButton removeParticipant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.participants);
+        removeParticipant=(ImageButton)findViewById(R.id.removeParticipant);
+        removeParticipant.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.removeParticipant:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Are you sure you want to remove this participant from the event?");
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Do nothing
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+        }
     }
 }
