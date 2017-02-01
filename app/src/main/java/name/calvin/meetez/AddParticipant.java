920b48a9dfee073cef11bf11d285fdd5c39bea1f
Matt1998 <tsekinping2010@gmail.com>
2017-02-01 14:32:35 +0800

    Bug fix for AddParticipant.java to return to previous activity after adding participant action is completed.

--- app/src/main/java/name/calvin/meetez/AddParticipant.java
+++ app/src/main/java/name/calvin/meetez/AddParticipant.java
@@ -1,14 +1,11 @@
 package name.calvin.meetez;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
@@ -40,11 +37,7 @@ public class AddParticipant extends Activity implements OnClickListener {
         switch (v.getId()) {
             case R.id.add:
                 Toast.makeText(getBaseContext(), "You just added new participant(s).", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), Participants.class);
-                if (intent != null) {
-                    startActivity(intent);
-                }
-
+                finish();
                 break;
         }
     }
